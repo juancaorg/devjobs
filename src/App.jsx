0ba2cc3f3de4dea75091ca43815a204c99cea56f
Juan Carrillo <juan@juanca.org>
2022-12-22 12:43:10 -0600

    feat(app): add Header component to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world! 👋</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
