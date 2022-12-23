f572f3e865cfcbc062ec5a1427a4e4cbb7486451
Juan Carrillo <juan@juanca.org>
2022-12-23 13:40:43 -0600

    feat(JobsGrid): pass props from JobsGrid to each JobCard

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,20 +1,30 @@
-import jobLogo from "../assets/logos/scoot.svg";
 import "./JobCard.css";
 
-function JobCard() {
+function JobCard({
+  company,
+  logo,
+  logoBackground,
+  position,
+  postedAt,
+  contract,
+  location,
+}) {
   return (
     <div className="main__job">
-      <div className="main__job--logo">
-        <img src={jobLogo} alt="" className="main__job--logo-img" />
+      <div
+        className="main__job--logo"
+        style={{ backgroundColor: logoBackground }}
+      >
+        <img src={logo} alt="" className="main__job--logo-img" />
       </div>
       <div className="main__job--info-wrapper">
-        <p className="main__job--posted-at">5h ago</p>
+        <p className="main__job--posted-at">{postedAt}</p>
         <p> • </p>
-        <p className="main__job--contract">Full Time</p>
+        <p className="main__job--contract">{contract}</p>
       </div>
-      <h1 className="main__job--position">Senior Software Engineer</h1>
-      <p className="main__job--company">Scoot</p>
-      <p className="main__job--location">United Kingdom</p>
+      <h1 className="main__job--position">{position}</h1>
+      <p className="main__job--company">{company}</p>
+      <p className="main__job--location">{location}</p>
     </div>
   );
 }
--- src/components/JobsGrid.jsx
+++ src/components/JobsGrid.jsx
@@ -4,18 +4,24 @@ import "./JobsGrid.css";
 function JobsGrid() {
   return (
     <main className="main">
-      <JobCard />
-      <JobCard />
-      <JobCard />
-      <JobCard />
-      <JobCard />
-      <JobCard />
-      <JobCard />
-      <JobCard />
-      <JobCard />
-      <JobCard />
-      <JobCard />
-      <JobCard />
+      <JobCard
+        company={"Scoot"}
+        logo={"/src/assets/logos/scoot.svg"}
+        logoBackground={"hsl(36, 87%, 49%)"}
+        position={"Senior Software Engineer"}
+        postedAt={"5h ago"}
+        contract={"Full Time"}
+        location={"United Kingdom"}
+      />
+      <JobCard
+        company={"Blogr"}
+        logo={"/src/assets/logos/blogr.svg"}
+        logoBackground={"hsl(13, 79%, 52%)"}
+        position={"Haskell and PureScript Dev"}
+        postedAt={"20h ago"}
+        contract={"Part Time"}
+        location={"United States"}
+      />
     </main>
   );
 }
