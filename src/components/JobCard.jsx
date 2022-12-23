1d6c708bb47f6cb5f0d4f493781d11b3e06dd0ae
Juan Carrillo <juan@juanca.org>
2022-12-23 10:55:08 -0600

    fix(JobCard): rename class to className

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -3,18 +3,18 @@ import "./JobCard.css";
 
 function JobCard() {
   return (
-    <div class="main__job">
+    <div className="main__job">
       <div className="main__job--logo">
-        <img src={jobLogo} alt="" class="main__job--logo-img" />
+        <img src={jobLogo} alt="" className="main__job--logo-img" />
       </div>
       <div className="main__job--info-wrapper">
-        <p class="main__job--posted-at">5h ago</p>
+        <p className="main__job--posted-at">5h ago</p>
         <p> • </p>
-        <p class="main__job--contract">Full Time</p>
+        <p className="main__job--contract">Full Time</p>
       </div>
-      <h1 class="main__job--position">Senior Software Engineer</h1>
-      <p class="main__job--company">Scoot</p>
-      <p class="main__job--location">United Kingdom</p>
+      <h1 className="main__job--position">Senior Software Engineer</h1>
+      <p className="main__job--company">Scoot</p>
+      <p className="main__job--location">United Kingdom</p>
     </div>
   );
 }
