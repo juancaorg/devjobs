433e3f1343faa0b13063e2c73cb6b002ee76c001
Juan Carrillo <juan@juanca.org>
2022-12-26 17:52:42 -0600

    fix(JobsGrid): show image logos in prod build

--- src/components/JobsGrid.jsx
+++ src/components/JobsGrid.jsx
@@ -2,7 +2,48 @@ import dataArr from "../assets/data.json";
 import JobCard from "./JobCard";
 import "./JobsGrid.css";
 
+// Logos 'absolute' import for the production build (npm run build).
+// For some reason, if I try to read the route without importing,
+// React doesn't import the files by itself on the prod build.
+// Strangely, in the CSS Header I don't use import for the image,
+// but it goes to the prod build anyway. Investigate this later.
+// Not ideal, but it works.
+import blogrLogo from "../assets/logos/blogr.svg";
+import coffeeroastersLogo from "../assets/logos/coffeeroasters.svg";
+import creativeLogo from "../assets/logos/creative.svg";
+import crowdfundLogo from "../assets/logos/crowdfund.svg";
+import makerLogo from "../assets/logos/maker.svg";
+import mastercraftLogo from "../assets/logos/mastercraft.svg";
+import officeliteLogo from "../assets/logos/officelite.svg";
+import podLogo from "../assets/logos/pod.svg";
+import pomodoroLogo from "../assets/logos/pomodoro.svg";
+import scootLogo from "../assets/logos/scoot.svg";
+import typemasterLogo from "../assets/logos/typemaster.svg";
+import vectorLogo from "../assets/logos/vector.svg";
+
 function JobsGrid() {
+  // Create logosObj for the logos routes.
+  // This is made so you don't have touse eval().
+  // For more info about not using eval, check MDN:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval#never_use_eval!
+
+  // Got the idea to use an object as an alternative from
+  // Delftstack: https://www.delftstack.com/howto/javascript/javascript-eval-alternative/
+  const logosObj = {
+    blogr: blogrLogo,
+    coffeeroasters: coffeeroastersLogo,
+    creative: creativeLogo,
+    crowdfund: crowdfundLogo,
+    maker: makerLogo,
+    mastercraft: mastercraftLogo,
+    officelite: officeliteLogo,
+    pod: podLogo,
+    pomodoro: pomodoroLogo,
+    scoot: scootLogo,
+    typemaster: typemasterLogo,
+    vector: vectorLogo,
+  };
+
   return (
     <main className="main">
       {dataArr.map((obj) => {
@@ -10,11 +51,7 @@ function JobsGrid() {
           <JobCard
             key={obj.id}
             company={obj.company}
-            logo={
-              "/src/assets/logos/" +
-              obj.company.split(" ").join("").toLowerCase() +
-              ".svg"
-            }
+            logo={logosObj[obj.company.split(" ").join("").toLowerCase()]}
             logoBackground={obj.logoBackground}
             position={obj.position}
             postedAt={obj.postedAt}
