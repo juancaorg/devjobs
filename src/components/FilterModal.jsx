0dbce426e1b2eae13a4486c256ec9379a1d16472
Juan Carrillo <juan@juanca.org>
2022-12-27 14:02:20 -0600

    feat(FilterModal): add FilterModal demo component

---
+++ src/components/FilterModal.jsx
@@ -0,0 +1,11 @@
+import "./FilterModal.css";
+
+function FilterModal() {
+  return (
+    <div className="header__search--filter-modal">
+      <h1>Hello window! 👋</h1>
+    </div>
+  );
+}
+
+export default FilterModal;
