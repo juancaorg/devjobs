92f8c2f190539ccdf09a15a74411128993409236
Juan Carrillo <juan@juanca.org>
2022-12-27 14:02:43 -0600

    feat(SearchBar): add Modal logic to SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 import filterIcon from "../assets/mobile/icon-filter.svg";
 import searchIcon from "../assets/desktop/icon-search.svg";
 import "./SearchBar.css";
 
+import Backdrop from "./Backdrop";
+import FilterModal from "./FilterModal";
+
 function SearchBar() {
+  const [showFilterModal, setShowFilterModal] = useState();
+
+  function showFilterModalHandler() {
+    setShowFilterModal(true);
+  }
+
+  function closeFilterModalHandler() {
+    setShowFilterModal(false);
+  }
+
   return (
     <div className="header__search">
       <input
@@ -12,12 +27,14 @@ function SearchBar() {
         className=""
         placeholder="Filter by title..."
       />
-      <button id="header__search--filter">
+      <button id="header__search--filter" onClick={showFilterModalHandler}>
         <img src={filterIcon} alt="" aria-hidden="true" />
       </button>
       <button type="submit" id="header__search--submit">
         <img src={searchIcon} alt="" aria-hidden="true" />
       </button>
+      {showFilterModal && <Backdrop onClick={closeFilterModalHandler} />}
+      {showFilterModal && <FilterModal />}
     </div>
   );
 }
