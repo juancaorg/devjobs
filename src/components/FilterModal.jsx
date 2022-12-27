e81917fd58f34df35540dbe1a47119132cc7eb8b
Juan Carrillo <juan@juanca.org>
2022-12-27 14:45:21 -0600

    feat(FilterModal): add location filter input

--- src/components/FilterModal.jsx
+++ src/components/FilterModal.jsx
@@ -1,9 +1,25 @@
+import locationIcon from "../assets/desktop/icon-location.svg";
 import "./FilterModal.css";
 
 function FilterModal() {
   return (
     <div className="header__search--filter-modal">
-      <h1>Hello window! 👋</h1>
+      <div className="header__search--filter-modal--wrapper">
+        <img
+          src={locationIcon}
+          alt=""
+          className="header__search--filter-modal--input-location-icon"
+          aria-hidden="true"
+        />
+        <input
+          type="search"
+          name="search"
+          id="header__search--filter-modal--input-location-icon"
+          className=""
+          placeholder="Filter by location..."
+        />
+      </div>
+      <h1>Hello modal! 👋</h1>
     </div>
   );
 }
