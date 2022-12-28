9255cfb9682cdec3b84821e87ba3623b396e77ed
Juan Carrillo <juan@juanca.org>
2022-12-27 18:08:53 -0600

    feat(Header): add conditionally render based on viewport size logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,33 @@
+import { useState, useEffect } from "react";
+
 import Logo from "./Logo";
 import ToggleSwitch from "./ToggleSwitch";
 import SearchBar from "./SearchBar";
 import "./Header.css";
 
 function Header() {
+  // React conditionally render based on viewport size.
+  // Credit to @foakesm on Stack Overflow:
+  // https://stackoverflow.com/questions/46586165/react-conditionally-render-based-on-viewport-size/60811141#60811141
+
+  const [isTablet, setTablet] = useState(window.innerWidth >= 768);
+
+  function updateMedia() {
+    setTablet(window.innerWidth >= 768);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
   return (
     <header className="header">
       <div className="header__wrapper">
         <Logo />
         <ToggleSwitch />
       </div>
-      <SearchBar />
+      {isTablet ? <div>I show on 768px or higher</div> : <SearchBar />}
     </header>
   );
 }
