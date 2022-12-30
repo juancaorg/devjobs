ff8eea43dbdc347800611e0f3cdde06470a556b1
Juan Carrillo <juan@juanca.org>
2022-12-30 16:55:24 -0600

    feat(App): move components to Home page and generate each job card Route and Detail demo page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import Header from "./components/Header";
-import JobsGrid from "./components/JobsGrid";
-import LoadMoreButton from "./components/LoadMoreButton";
-import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+import dataArr from "./assets/data.json";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Header />
-      <JobsGrid />
-      <LoadMoreButton />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {dataArr.map((obj) => {
+          const routesList = (
+            <Route
+              key={obj.id}
+              path={`${obj.company}-${obj.id}/`
+                .split(" ")
+                .join("")
+                .toLowerCase()}
+              element={<Detail position={obj.position} />}
+            />
+          );
+          return routesList;
+        })}
+      </Routes>
     </>
   );
 }
