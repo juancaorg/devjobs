5d438152f2387497c615c29d10f69917a2e3b349
Juan Carrillo <juan@juanca.org>
2022-12-30 15:41:17 -0600

    feat(Detail): add basic Detail page for each job inner page to be generated

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,12 @@
+import "./Detail.css";
+
+function Detail({ position }) {
+  return (
+    <div className="detail">
+      <h1>Hello!</h1>
+      <p>{position}</p>
+    </div>
+  );
+}
+
+export default Detail;
