4fc49fcd3ddfeeafe8d5cc5bddd844061d084ab0
Juan Carrillo <juan@juanca.org>
2023-01-01 16:40:54 -0600

    feat(CompanyDetail): add structure and styling for CompanyDetail component (mobile)

--- src/components/CompanyDetail.jsx
+++ src/components/CompanyDetail.jsx
@@ -1,7 +1,34 @@
 import "./CompanyDetail.css";
 
 function CompanyDetail({ company, logo, logoBackground, website }) {
-  return <h1>CompanyDetail component</h1>;
+  return (
+    <div className="company__detail">
+      <div
+        className="company__detail--logo"
+        style={{ backgroundColor: logoBackground }}
+      >
+        <img
+          src={logo}
+          alt={company + " logo"}
+          className="company__detail--logo-img"
+        />
+      </div>
+      <div className="company__detail--info-wrapper">
+        <h1 className="company__detail--name">{company}</h1>
+        <p className="company__detail--website">
+          {company.split(" ").join("").toLowerCase() + ".com"}
+        </p>
+      </div>
+      <a
+        href={website}
+        rel="noopener"
+        target="_blank"
+        className="company__detail--link"
+      >
+        Company Site
+      </a>
+    </div>
+  );
 }
 
 export default CompanyDetail;
