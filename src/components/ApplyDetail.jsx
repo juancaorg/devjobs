acb6d2414e230fd6f6f086a8ec6c1ad973a387a7
Juan Carrillo <juan@juanca.org>
2023-01-02 20:01:11 -0600

    feat(ApplyDetail): add mobile, tablet, desktop view

--- src/components/ApplyDetail.jsx
+++ src/components/ApplyDetail.jsx
@@ -6,7 +6,7 @@ function ApplyDetail({ company, position, apply, className }) {
     <div className={`apply__detail ${className}`}>
       <div className="apply__detail--wrapper">
         <h1 className="apply__detail--position">{position}</h1>
-        <p className="apply__detail--company">{company}</p>
+        <p className="apply__detail--company">{`${company} Inc.`}</p>
       </div>
       <ApplyButton apply={apply} className={"apply__detail--button"} />
     </div>
