6e00da3c9dd014e11404091af790aaff47586612
Juan Carrillo <juan@juanca.org>
2023-01-02 19:51:20 -0600

    feat(ApplyDetail): add mobile view

--- src/components/ApplyDetail.jsx
+++ src/components/ApplyDetail.jsx
@@ -3,13 +3,13 @@ import "./ApplyDetail.css";
 
 function ApplyDetail({ company, position, apply, className }) {
   return (
-    <>
-      <div className={`apply__detail--wrapper ${className}`}>
+    <div className={`apply__detail ${className}`}>
+      <div className="apply__detail--wrapper">
         <h1 className="apply__detail--position">{position}</h1>
         <p className="apply__detail--company">{company}</p>
       </div>
-      <ApplyButton apply={apply} />
-    </>
+      <ApplyButton apply={apply} className={"apply__detail--button"} />
+    </div>
   );
 }
 
